f96fe5770c132f35257b0fedc01837ec4691c793
Pai <phagcartorn@gmail.com>
2018-08-14 14:40:02 +0700

    CORS

--- server/server.js
+++ server/server.js
@@ -8,6 +8,20 @@ var { opentok } = require('./openTox/opentox');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
 
