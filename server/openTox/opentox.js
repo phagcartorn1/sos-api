75ca2b0ef991b26a1bb36a8bd479d213a6f694ba
Pai <phagcartorn@gmail.com>
2018-08-14 10:39:06 +0700

    add toxbo

---
+++ server/openTox/opentox.js
@@ -0,0 +1,7 @@
+var OpenTok = require('opentok');
+const apiKey = '46165962';
+const apiSecret = '29949ed67984643123a5223cabd47a8fb90e36ff';
+var opentok = new OpenTok(apiKey, apiSecret);
+
+
+module.exports = {opentok}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {user} = require('./models/user');
+//var {opentox} = require('./openTox/opentox');
+
+var OpenTok = require('opentok');
+const apiKey = '46165962';
+const apiSecret = '29949ed67984643123a5223cabd47a8fb90e36ff';
+var opentok = new OpenTok(apiKey, apiSecret);
 
 const port = process.env.PORT || 3000;
 
@@ -17,6 +23,43 @@ app.get('/login',(req,res)=>{
     res.send({status:"ok"});
 
 });
+
+
+app.post('/call',(req,res)=>{
+    
+
+    
+    
+    opentok.createSession((err, session)=>{
+
+        if(err){
+            res.status(400).send({
+                stauts : '400',
+                data : null,
+                message: 'open tox can not generate session',
+                error : err
+            })
+        }
+        else
+        {
+            var toxToken = session.generateToken();
+            res.status(200).send({
+                stauts : '200',
+                data : {token:toxToken,sessionID:session.sessionId},
+                message: 'open tox generate token success',
+                error : null
+            })
+        }
+
+
+    });
+
+
+
+});
+
+
+
 
 
 
